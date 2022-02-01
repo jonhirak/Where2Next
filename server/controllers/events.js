67ac530115e961ec2f05ef4005199a157ffc989e
Takahiro Hirakawa <jonhirak1992@gmail.com>
2022-01-31 17:29:57 -0800

    Fix bug in events.js

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 // const { EVENT_API_KEY } = require("../../config.js");
+require('dotenv').config()
 const zipcodes = require("zipcodes");
 module.exports = {
   getNearbyEvents: (req, res) => {
@@ -8,12 +9,9 @@ module.exports = {
     const start = new Date(startDate).toISOString().slice(0, -5) + "Z";
     const end = new Date(endDate).toISOString().slice(0, -5) + "Z";
 
-    console.log('TEST' + process.env.REACT_APP_EVENT_API_KEY)
-    console.log('TEST2' + REACT_APP_EVENT_API_KEY)
-
     axios
       .get(
-        `https://app.ticketmaster.com/discovery/v2/events.json?latlong=${latitude},${longitude}&startDateTime=${start}&endDateTime=${end}&radius=30&unit=miles&apikey=${process.env.REACT_APP_EVENT_API_KEY}&size=100&sort=date,asc`
+        `https://app.ticketmaster.com/discovery/v2/events.json?latlong=${latitude},${longitude}&startDateTime=${start}&endDateTime=${end}&radius=30&unit=miles&apikey=${process.env.REACT_APP__EVENT_API_KEY}&size=100&sort=date,asc`
       )
       .then((d) => {
         const events = d.data._embedded ? d.data._embedded.events : [];
