284328bf503dba5fb01987da66169064a1be47e9
Takahiro Hirakawa <jonhirak1992@gmail.com>
2022-01-31 20:21:47 -0800

    Fix typo in events.js

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -9,11 +9,9 @@ module.exports = {
     const start = new Date(startDate).toISOString().slice(0, -5) + "Z";
     const end = new Date(endDate).toISOString().slice(0, -5) + "Z";
 
-    console.log('URL = ' + `https://app.ticketmaster.com/discovery/v2/events.json?latlong=${latitude},${longitude}&startDateTime=${start}&endDateTime=${end}&radius=30&unit=miles&apikey=${EVENT_API_KEY}&size=100&sort=date,asc`);
-
     axios
       .get(
-        `https://app.ticketmaster.com/discovery/v2/events.json?latlong=${latitude},${longitude}&startDateTime=${start}&endDateTime=${end}&radius=30&unit=miles&apikey=${REACT_APP_EVENT_API_KEY}&size=100&sort=date,asc`
+        `https://app.ticketmaster.com/discovery/v2/events.json?latlong=${latitude},${longitude}&startDateTime=${start}&endDateTime=${end}&radius=30&unit=miles&apikey=${EVENT_API_KEY}&size=100&sort=date,asc`
       )
       .then((d) => {
         const events = d.data._embedded ? d.data._embedded.events : [];
